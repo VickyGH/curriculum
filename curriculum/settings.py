90a7fd639194166d0dd42bdae0ce521cb04db68b
Vicky García Hernández <VickyGH-MAC@MBP-de-Vicky.lan>
2017-01-03 22:43:26 -0600

    prueba

--- curriculum/settings.py
+++ curriculum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-cc(b%@6aoe7+a0*7-5565zb-@7a+adospbgm^zjy%)0&ldm1y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
