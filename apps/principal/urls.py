9fa070b679a01c59c587cde2553c33d7e173211e
Vicky Garcia Hernandez <gar_vicky93@hotmail.com>
2017-01-07 19:33:28 -0600

    Asignar Estilo

--- apps/principal/urls.py
+++ apps/principal/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import url
 
 from .views import *
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     url(r'^$', inicio.as_view(), name='Inicio'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
